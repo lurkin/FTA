2735b5e2a648cd898eda58d80c677f9788c442cc
xXxennoxXx <kubaki_96@wp.pl>
2020-02-20 00:49:10 +0100

    add evalute of request

--- Server/src/main/java/handler/Handler.java
+++ Server/src/main/java/handler/Handler.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public class Handler implements Callable<Boolean> {
@@ -21,15 +23,19 @@ public class Handler implements Callable<Boolean> {
         try (BufferedReader msgClientToServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter msgServerToClient = new PrintWriter(socket.getOutputStream(), true)) {
 
-            msgServerToClient.write("Proper listening appeared.");
-
-            msgClientToServer.readLine();
-
+            evaluateRequest(msgClientToServer.readLine());
 
 
         }
 
 
         return null;
+    }
+
+    public void evaluateRequest(String request) {
+        List<String> splittedRequest = Arrays.asList(request.split(" "));
+        switch (splittedRequest.remove(0)){
+
+        }
     }
 }
